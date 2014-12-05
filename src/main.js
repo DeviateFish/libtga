0c92218f4384cedccdba3ca155673f0eccd44436
DeviateFish <savorydeviatedelight@gmail.com>
2014-12-04 20:54:02 -0800

    initial commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+/* libtga main */
+
+// Base function.
+var libtga = function() {
+  // Add functionality here.
+  return true;
+};
+
+
+// Version.
+libtga.VERSION = '0.0.0';
+
+
+// Export to the root, which is probably `window`.
+root.libtga = libtga;
